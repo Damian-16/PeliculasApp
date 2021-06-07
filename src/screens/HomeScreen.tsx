0ac8d61e5b60491b916c90feea68b2b02584eff3
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-06-07 01:07:26 -0400

    se agrego una dependencia de carrusel y se hizo la configuracion para renderizarla

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import { useNavigation } from '@react-navigation/core';
-import React, { useEffect } from 'react'
-import { ActivityIndicator, ActivityIndicatorBase, Button, View } from 'react-native'
-import movieDB from '../api/movieDB';
+import React from 'react'
+import { ActivityIndicator, ActivityIndicatorBase, Button, Dimensions, View } from 'react-native'
 import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 import { useMovies } from '../hooks/useMovies';
 import { MoviePoster } from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
 
+
+const windowWidth = Dimensions.get('window').width
 
 export const HomeScreen = () => {
     const {peliculasEnCine,IsLoading} = useMovies();
@@ -22,12 +24,23 @@ export const HomeScreen = () => {
     }
     return (
         <View style={{marginTop: top + 20}}> 
-            <MoviePoster
+            {/* <MoviePoster
             movie={peliculasEnCine[1]}/>
             <Button 
             title="ir detalle"
             onPress={()=>navigation.navigate('DetailScreen')}></Button>
-            
+             */}
+            <View style={{
+            height:340,
+            // backgroundColor:'red'
+        }} >
+             <Carousel 
+             data={peliculasEnCine}
+             renderItem={({item}:any)=> <MoviePoster movie={item}/>}
+             sliderWidth={windowWidth}
+             itemWidth={180}
+             />
+             </View>
         </View>
     )
 }
